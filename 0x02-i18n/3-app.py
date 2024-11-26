bbd25597a45ba8c53073f5e2a8a541e787779cf8
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-11-26 21:12:42 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel and template translations
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Configuration class for the Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Basic route for the application.
+    """
+    return render_template('3-index.html', home_title=_("home_title"),
+                           home_header=_("home_header"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
