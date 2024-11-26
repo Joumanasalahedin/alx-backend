ab323ccccfaa736079cd308d57f36419133a3ed4
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-11-26 21:02:26 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Flask app with Babel extension for internationalization
+Flask app with Babel extension and locale selection
 """
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
 
@@ -22,12 +22,20 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index() -> str:
     """
     Basic route for the application.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
